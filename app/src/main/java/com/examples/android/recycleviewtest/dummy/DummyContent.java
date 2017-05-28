24c27bc2744c928f5e0f0e9101bce8e4f0e5e2aa
jcramossa <jcramossa@gmail.com>
2017-05-28 11:51:33 +0200

    Comentario 1 en la rama master

--- app/src/main/java/com/examples/android/recycleviewtest/dummy/DummyContent.java
+++ app/src/main/java/com/examples/android/recycleviewtest/dummy/DummyContent.java
@@ -11,7 +11,9 @@ import java.util.Map;
  * <p>
  * TODO: Replace all uses of this class before publishing your app.
  */
-public class DummyContent {
+
+
+public class DummyContent {  // Comentario en la rama master
 
     /**
      * An array of sample (dummy) items.
