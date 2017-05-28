6bacb5e879824a05bb75a3dd3c8f4bd7a43215dd
jcramossa <jcramossa@gmail.com>
2017-05-28 11:48:33 +0200

    Primera modificacion en la clase DummyConten1

--- app/src/main/java/com/examples/android/recycleviewtest/dummy/DummyContent1.java
+++ app/src/main/java/com/examples/android/recycleviewtest/dummy/DummyContent1.java
@@ -2,6 +2,7 @@ package com.examples.android.recycleviewtest.dummy;
 
 /**
  * Created by jcramossa on 28/05/17.
+ * Primera modificacion en la clase DummyContent1
  */
 
 public class DummyContent1 {
